06e05af6826a0493b6519959e7f326cb9893466c
Monika Urbanska <monika.urbanska1@gmail.com>
2020-02-02 17:22:56 +0000

    updated to respond with own message

--- handler.js
+++ handler.js
@@ -5,8 +5,7 @@ module.exports.tasks = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'Your API works',
       },
       null,
       2

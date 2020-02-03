3cfae7a4b53c41db6dba47cf5c2358396d455a11
Monika Urbanska <monika.urbanska1@gmail.com>
2020-02-03 20:07:58 +0000

    updating serverless to use express

--- handler.js
+++ handler.js
@@ -1,17 +1,13 @@
 'use strict';
 
-module.exports.tasks = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Your API works',
-      },
-      null,
-      2
-    ),
-  };
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+app.get('/tasks', function (req, res) {
+  res.send('Hello World!')
+})
+
+module.exports.tasks = serverless(app);
+
+

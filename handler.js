ff7b7ece652b535b420823028e5014604982e4fa
Monika Urbanska <monika.urbanska1@gmail.com>
2020-02-11 19:21:03 +0000

    updated to return array of tasks

--- handler.js
+++ handler.js
@@ -1,13 +1,16 @@
 'use strict';
 
+const uuidv4 = require('uuid/v4');
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
 app.get('/tasks', function (req, res) {
-  res.json({
-    message: 'Your API works',
-  });
+  res.json([
+    { id: uuidv4(), description: "Walk the dog", completed: false },
+    { id: uuidv4(), description: "Water plants", completed: false },
+    { id: uuidv4(), description: "Buy milk", completed: false }
+  ]);
 });
 
 module.exports.tasks = serverless(app);

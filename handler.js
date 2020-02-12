d42132e9d615293789d8deb2a25535e69bed8072
Monika Urbanska <monika.urbanska1@gmail.com>
2020-02-12 15:18:12 +0000

    connecting to database, adding put, post, delete

--- handler.js
+++ handler.js
@@ -1,20 +1,51 @@
 'use strict';
 
-const uuidv4 = require('uuid/v4');
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
+const uuidv4 = require('uuid/v4');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host : 'instance',
+  user : '',
+  password : '',
+  database: 'todo_app'
+});
 
 app.get('/tasks', function (req, res) {
   
-  const someTasks = [
-    { id: uuidv4(), description: "Walk the dog", completed: false },
-    { id: uuidv4(), description: "Water plants", completed: false },
-    { id: uuidv4(), description: "Buy milk", completed: false }
-  ]
-  
+  connection.query('SELECT * FROM `tasks` WHERE `userId` = 2', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if(error) {
+      console.error("Your query had a problem with fetching tasks", error);
+      res.status(500).json({errorMessage: error});
+    }
+    else {
+      // Query was successful
+      res.json({tasks: results});
+    }
+  });
+});
+
+// Creating tasks
+app.post('/tasks', function (req, res) {
   res.json({
-    tasks: someTasks
+    message: 'Your POST works',
+  });
+});
+
+// Updating tasks
+app.put('/tasks/:taskId', function (req, res) {
+  res.json({
+    message: 'Your PUT works',
+  });
+});
+
+// Deleting tasks
+app.delete('/tasks/:taskId', function (req, res) {
+  res.json({
+    message: 'Your DELETE works',
   });
 });
 

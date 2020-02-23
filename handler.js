48a8bcace9ceda847aec607bd7f30376ff446f28
Monika Urbanska <monika.urbanska1@gmail.com>
2020-02-23 15:49:17 +0000

    Successful creation of tasks

--- handler.js
+++ handler.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use(express.json());
 const uuidv4 = require('uuid/v4');
 const mysql = require('mysql');
 
@@ -13,6 +14,7 @@ const connection = mysql.createConnection({
   database: process.env.DB_SCHEMA
 });
 
+// Retrieving tasks
 app.get('/tasks', function (req, res) {
   
   connection.query('SELECT * FROM `tasks` WHERE `userId` = 2', function (error, results, fields) {
@@ -30,8 +32,26 @@ app.get('/tasks', function (req, res) {
 
 // Creating tasks
 app.post('/tasks', function (req, res) {
-  res.json({
-    message: 'Your POST works',
+
+  // Accept info from client about what task is being created
+
+  const taskToInsert = req.body;
+  taskToInsert.taskId = uuidv4();
+
+  // Take that info, pre-populate an SQL INSERT statement
+  // Execute statement
+
+  connection.query('INSERT INTO `tasks` SET ?', taskToInsert, function (error, results, fields) {
+    if(error) {
+      console.error("Your query had a problem with inserting a new task", error);
+      res.status(500).json({errorMessage: error});
+    }
+    else {
+      // Return to client info about task that has been created
+      res.json({
+        task: taskToInsert
+      });
+    }
   });
 });
 
